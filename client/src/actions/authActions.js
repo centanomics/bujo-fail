6e6285baed984b565e0a328964009b7e10eb3b58
semyers189 <semyers@fullsail.edu>
2021-01-18 08:32:48 -0500

    added register user function

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,34 @@
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  USER_LOADED,
+  AUTH_ERROR
+} from './types';
+
+import API from '../utils/API';
+
+// register user
+
+export const registerUser = formData => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    const res = API.post('/api/auth/register', formData)
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data
+    })
+    
+  } catch (err) {
+    dispatch({
+      type: REGISTER_FAIL,
+      payload: err.toString(),
+    })
+  }
+}
