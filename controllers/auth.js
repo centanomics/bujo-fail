03d10215bfec6c634878ff6c4dbcd8c1d6490e42
semyers189 <semyers@fullsail.edu>
2021-01-20 22:02:04 -0500

    added email check

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,24 @@
 const { validationResult } = require('express-validator');
+const Users = require('../models/users');
 
 // register user
 
 exports.registerUser = async (req, res) => {
+  // if there are errors in the input, returns those errors
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  res.status(200).json({"message": "hi"})
+  const { firstName, lastName, email, password, password2 } = req.body;
+
+  try {
+    let user = await Users.findOne({ email: email })
+    if (user) {
+      return res.status(400).json({msg: 'User already exist'})
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 }
