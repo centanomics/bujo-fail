8e47d7ba7f448bf7a85e04f6f65b4186cee16f79
semyers189 <semyers@fullsail.edu>
2021-01-21 21:22:26 -0500

    hashed password and created document for each registered user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
 const Users = require('../models/users');
 
 // register user
@@ -7,16 +9,29 @@ exports.registerUser = async (req, res) => {
   // if there are errors in the input, returns those errors
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    res.status(400).json({ errors: errors.array() });
   }
 
-  const { firstName, lastName, email, password, password2 } = req.body;
+  const { firstName, lastName, email, password } = req.body;
 
   try {
-    let user = await Users.findOne({ email: email })
+    // checks to see if the user exists
+    let user = await Users.find({ email })
+    console.log(user)
     if (user) {
       return res.status(400).json({msg: 'User already exist'})
     }
+
+    let salt = bcrypt.genSaltSync(10);
+    user = new Users({
+      firstName,
+      lastName,
+      email,
+      password: bcrypt.hashSync(password, salt),
+    })
+
+
+    res.status(200).json({msg: 'hi'})
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
