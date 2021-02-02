dba62f3a80778ab904c41baa5a44b389894609f5
semyers189 <semyers@fullsail.edu>
2021-02-01 22:02:24 -0500

    added get user on clientside

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -4,11 +4,32 @@ import {
   // LOGIN_SUCCESS,
   // LOGIN_FAIL,
   // LOGOUT,
-  // USER_LOADED,
-  // AUTH_ERROR
+  AUTH_ERROR,
+  USER_LOADED
 } from './types';
+import setAuthToken from '../utils/setAuthToken';
 
 import API from '../utils/API';
+
+// loads user
+
+export const loadUser = () => async dispatch => {
+  if (localStorage.token) setAuthToken(localStorage.token);
+
+  try {
+    const res = await API.get('/api/auth');
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    })
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR
+    });
+  }
+
+}
 
 // register user
 
