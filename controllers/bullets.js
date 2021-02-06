5bec3a11744afdeb045e99df14b2e4e384554f6d
semyers189 <semyers@fullsail.edu>
2021-02-05 22:06:32 -0500

    added get bullets

--- controllers/bullets.js
+++ controllers/bullets.js
@@ -1,5 +1,12 @@
+const Bullets = require('../models/bullets');
 
 // get bullets from one userid
 exports.getAllBullets = async (req, res) => {
-
+  try {
+    const bullets = Bullets.find({ userId: req.body.userId });
+    res.json(bullets)
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 }
